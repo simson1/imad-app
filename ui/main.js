3b9933bad3d92fee8223946f0326d0b68d2ad2f9
Jay Dave <jaydave422@gmail.com>
2017-12-22 23:25:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
-console.log('Loaded!');
+var counter=document.getElementById('click');
+var counter=0;
+counter.onclick=function(){
+    
+    counter=counter+1;
+    var c=document.getElementById('j');
+    c.innerHTML=counter.toString();
+    
+    
+    
+}
