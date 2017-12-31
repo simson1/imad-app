6111d57d09bb87dd95b90d7bc3022e7559a13fcd
Jay Dave <jaydave422@gmail.com>
2017-12-31 23:11:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,25 @@ $('.main').click(function() {
 		left:"0px"
 	},100);
 	});
+  
+
+
+/*counter.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+    if(request.readyState==XMLHttpRequest.DONE){
+        if(request.status==200){
+            var count = request.responseText;
+            var c=document.getElementById('j');
+            c.innerHTML=count.toString();
+            
+        }
+        
+    }
+        
+    };
+
+
+    request.open('GET','http://jaydave422.imad.hasura-app.io/ui/cout',true);
+    request.send(null);
+};*/
